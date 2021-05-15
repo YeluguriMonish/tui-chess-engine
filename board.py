c2dbb99ae47184ad20829b682447bc52a63253c4
Monish Yeluguri <yeluguri.monish@gmail.com>
2021-05-15 10:59:09 -0400

    engine initialized but not implemented

--- board.py
+++ board.py
@@ -1,21 +1,31 @@
 import chess
+import chess.engine
 from blessed import Terminal
 
 term = Terminal()
 board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+engine = chess.engine.SimpleEngine.popen_uci(
+    "./Stockfish-master/src/stockfish")
+engine.close()
 
-with term.cbreak(), term.hidden_cursor():
-    exit = False
-    inputting = True
+with term.cbreak(), term.hidden_cursor(), term.fullscreen():
     move = ""
-    while inputting:
+    while True:
         print(term.home + term.clear)
         print(board)
         print("enter move: " + move)
         inp = term.inkey()
-        if inp.code != 343:
+        if inp.isdigit() or inp.isalpha():
             move = move + inp
+        if inp.code == 263:
+            move = move[:-1]
+        if inp == 'q':
+            break
         if inp.code == 343:
-            x = chess.Move.from_uci(move)
-            board.push(x)
+            try:
+                x = chess.Move.from_uci(move)
+                if x in board.legal_moves:
+                    board.push(x)
+            except ValueError:
+                pass
             move = ""
